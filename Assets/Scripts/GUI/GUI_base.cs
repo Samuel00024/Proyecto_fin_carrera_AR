c9e4ef6c6a9ba850df77e00bb0f6712be78a97ec
Samuel00024 <sml00024@red.ujaen.es>
2016-08-14 13:54:44 +0200

    Prueba GUI v. 0.1

---
+++ Assets/Scripts/GUI/GUI_base.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUI_base : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI(){
+
+		//GUI.backgroundColor = Color.yellow;
+		//GUI.color = Color.yellow;
+		//GUI.Box (new Rect (10, 10, 100, 100), "Marcador");
+
+	}
+}
