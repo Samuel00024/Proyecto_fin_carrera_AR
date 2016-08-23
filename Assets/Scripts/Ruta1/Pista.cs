8a8faf8869e78840290ff9bff3c0a25dc4248c3b
Samuel00024 <sml00024@red.ujaen.es>
2016-08-23 03:07:36 +0200

    GUI v 0.1.4 - Faltaban archivos que añadir al commit anterior

---
+++ Assets/Scripts/Ruta1/Pista.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pista : MonoBehaviour {
+
+	private string poema="          - Bernardo López García -\n\nLos años pasando van,\ny el templo su mole ostenta;\nlo que por Dios se sustenta\nlos años no lo hundirán.\n\nCorren y corren edades\njunto a la Iglesia grandiosa;\npor su cúpula ostentosa\nresbalan las tempestades,\n\nY eterna y firme levanta\nsu continente sereno;\nni la hace temblar el trueno,\nni la muerte la quebranta.\n\nY es porque la alta piedad\nlos frutos del bien aprueba;\ny lo que por Dios se eleva,\ntiene luz de eternidad.";
+	private string place="Tu siguiente destino es:\n\nLA CATEDRAL DE JAÉN";
+
+	//Tiempo de espera hasta que desaparezca la pista y aparezca el lugar al que dirigirse de forma directa
+	//Para evitar que alguien no pueda realizar el circuito.
+	//Penalización por tiempo.
+	public double nextPlaceTimer;
+
+	private Text texto;
+	private GameObject aceptar;
+
+	private int i, j, k;
+
+	private System.TimeSpan endTime;
+
+	// Use this for initialization
+	void Start () {
+
+		texto = GameObject.Find ("PistaText").GetComponent<Text>();
+		aceptar = GameObject.Find ("OkButton");
+		aceptar.SetActive (false);
+		i = 0;
+		j = -1;
+		k = 0;
+
+		endTime = System.TimeSpan.FromSeconds (Time.time) + System.TimeSpan.FromSeconds(nextPlaceTimer);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (System.TimeSpan.FromSeconds (Time.time) >= endTime) {
+
+			nextPlace ();
+
+		}else if(!texto.text.Equals(poema)){
+
+			j++;
+
+			if (j >= 2) {
+
+				if (poema [i] == '\n' && k < 20) {
+
+					k++;
+
+				}
+				else {
+					
+					insertOnText ();
+					j = 0;
+					k = 0;
+				}
+			}
+		}
+
+	}
+
+	void insertOnText(){
+
+		texto.text = texto.text + poema [i];
+		i++;
+
+		if (texto.text.Equals (poema)) {
+
+			endButton ();
+
+		}
+	}
+
+	void nextPlace(){
+
+		texto.text = place;
+
+	}
+
+	void endButton(){
+
+		aceptar.SetActive (true);
+
+	}
+}
--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,15 +6,11 @@ using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour {
 
 	public Text guiText;
-	public Camera cam;
-	public GameObject background;
-	//private System.TimeSpan timeTemp = new System.TimeSpan(0,59,56);
 
 	// Use this for initialization
 	void Start () {
 
 		guiText.color = Color.yellow;
-		//guiText.transform.localPosition = new Vector2 (-(cam.pixelWidth/2), (cam.pixelHeight/2));
 		
 	}
 	
@@ -22,7 +18,6 @@ public class Timer : MonoBehaviour {
 	void Update () {
 
 		System.TimeSpan time = System.TimeSpan.FromSeconds (Time.timeSinceLevelLoad);
-		//time = time+timeTemp;
 		guiText.text =
 			(time.Hours).ToString ("00")
 			+ ":"
